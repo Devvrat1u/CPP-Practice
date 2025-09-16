e57863ad35b1a0608ac59dd44d4ebbcd9274a9d8
DevvratShukla <devshukla1818@gmail.com>
2025-09-16 09:36:17 +0530

    AddTwoInteger

---
+++ AddTwoIntegers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+
+    cout<<"\nEnter first number: ";
+    cin>>a;
+    cout<<"\nEnter second number: ";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<"\nSum is: "<<c;
+
+}
