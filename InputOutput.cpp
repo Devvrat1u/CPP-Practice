0a57f3086a01f08f2f850d608dbd59fb82c3fc4e
DevvratShukla <devshukla1818@gmail.com>
2025-09-16 09:40:29 +0530

    InputOutput

---
+++ InputOutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    int age;
+
+    cout<<"\nEnter your name: ";
+    cin>>name;
+    cout<<"\nEnter your age: ";
+    cin>>age;
+
+    cout<<"\nYour name is "<<name;
+    cout<<"\nYour age is "<<age;
+}
